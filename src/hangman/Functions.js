9ffc97859063128245b750b8ca783dc2f786249f
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-01 17:14:28 +0200

    Adds nice hangman image

--- src/hangman/Functions.js
+++ src/hangman/Functions.js
@@ -1,3 +1,20 @@
+import image0 from '../images/hangman0.png'
+import image1 from '../images/hangman1.png'
+import image2 from '../images/hangman2.png'
+import image3 from '../images/hangman3.png'
+import image4 from '../images/hangman4.png'
+import image5 from '../images/hangman5.png'
+import image6 from '../images/hangman6.png'
+import image7 from '../images/hangman7.png'
+import image8 from '../images/hangman8.png'
+import image9 from '../images/hangman9.png'
+
+export function returnImage(index){
+  const images = [image0, image1, image2, image3, image4, image5, image6, image7, image8, image9]
+  var currentImage = images[index]
+  return currentImage
+}
+
 export function wrongGuessCount(currentWord, currentGuesses) {
     return currentGuesses.reduce(function(sum, guess) {
       return sum + (currentWord.indexOf(guess, 0) === -1)
@@ -18,14 +35,14 @@ export function isWinner(currentWord, currentGuesses) {
   var wrong = wrongGuessCount(currentWord, currentGuesses)
   var answer = showGuess(currentWord, currentGuesses)
 
-  if (wrong <= 6 && answer === currentWord.split("").join(" "))
+  if (wrong <= 9 && answer === currentWord.split("").join(" "))
     return true
   else
     return false
 }
 
 export function isLoser(currentWord, currentGuesses){
-  if (wrongGuessCount(currentWord, currentGuesses) > 5)
+  if (wrongGuessCount(currentWord, currentGuesses) > 8)
     return true
   else
     return
--- src/hangman/Hangman.js
+++ src/hangman/Hangman.js
@@ -3,31 +3,18 @@ import { connect } from 'react-redux'
 import { wrongGuessCount } from './Functions'
 import { isWinner } from './Functions'
 import { isLoser } from './Functions'
+import { returnImage } from './Functions'
 
 export class Hangman extends PureComponent {
   render() {
     const currentWord = this.props.word
     const currentGuesses = this.props.guesses
     const wrongGuesses = wrongGuessCount(currentWord, currentGuesses)
-    const winState = isWinner(currentWord, currentGuesses)
-    const loss = isLoser(currentWord, currentGuesses)
 
-    if (winState === true) return (
-      <div className="hangman">
-        <h1>You win :D </h1>
-      </div>
-    )
+    return (
 
-    else if (loss === true) return (
-      <div className="hangman">
-        <h1>You lose :(</h1>
-      </div>
-    )
+        <img src={returnImage(wrongGuesses)} alt={wrongGuesses} />
 
-    else return (
-      <div className="hangman">
-        <h1>{`${wrongGuesses}/6 wrong guesses`}</h1>
-      </div>
     )
   }
 }
